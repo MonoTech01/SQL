74e39b7d8f67ac4d2be1b98b392822ff8347eaa2
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2025-03-24 10:23:36 -0700

    Update Create LessonSchedule table with FK constraints.sql

--- Labs/Create LessonSchedule table with FK constraints.sql
+++ Labs/Create LessonSchedule table with FK constraints.sql
@@ -1,1 +1,18 @@
+/*The database contains a Horse table, with columns:
 
+ID - integer, primary key
+RegisteredName - variable-length string
+The database contains a Student table, with columns:
+
+ID - integer, primary key
+FirstName - variable-length string
+LastName - variable-length string
+Create a third table, named LessonSchedule, with columns:
+
+HorseID - integer with range 0 to 65 thousand, not NULL, foreign key references Horse(ID)
+StudentID - integer with range 0 to 65 thousand, foreign key references Student(ID)
+LessonDateTime - date/time, not NULL
+Primary key is (HorseID, LessonDateTime)
+If a row is deleted from Horse, the rows with the same horse ID should be deleted from LessonSchedule automatically.
+
+If a row is deleted from Student, the same student IDs should be set to NULL in LessonSchedule automatically.*/
