80c85605bb1b95d0f513b4a47fd7202a99d8e424
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2025-03-24 09:52:56 -0700

    Update Select horses with logical operators.sql

--- Labs/Select horses with logical operators.sql
+++ Labs/Select horses with logical operators.sql
@@ -6,3 +6,8 @@ Breed - variable-length string
 Height - decimal number
 BirthDate - date
 Write a SELECT statement to select the registered name, height, and birth date for only horses that have a height between 15.0 and 16.0 (inclusive) or have a birth date on or after January 1, 2020.*/
+
+select RegisteredName, Height, BirthDate
+from Horse
+where (Height between 15.0 and 16.0)
+or (BirthDate >="2020-01-01")
