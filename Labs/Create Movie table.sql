defa52a0f262c8cc50e4e8a052c1d82a0b011e10
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2025-03-24 09:55:31 -0700

    Update Create Movie table.sql

--- Labs/Create Movie table.sql
+++ Labs/Create Movie table.sql
@@ -1,1 +1,7 @@
+/*Create a Movie table with the following columns:
 
+ID - positive integer with maximum value of 65,535
+Title - variable-length string with up to 50 characters
+Rating - fixed-length string with 4 characters
+ReleaseDate - date
+Budget - decimal value representing a cost of up to 999,999 dollars, with 2 digits for cents*/
