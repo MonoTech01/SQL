83f003801047bc5714beb4a578f269969af9f3d9
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2025-03-24 10:58:38 -0700

    Create Rollback and savepoint.sql

---
+++ Labs/Rollback and savepoint.sql
@@ -0,0 +1,11 @@
+/*Refer to the actor table of the Sakila database. The table in this lab has the same columns and data types but fewer rows.
+
+Start a transaction and:
+
+Insert a new actor with values 999, 'NICOLE', 'STREEP', '2021-06-01 12:00:00'
+Set a SAVEPOINT.
+Delete the actor with first name 'CUBA'.
+Select all actors.
+Roll back to the savepoint.
+Select all actors a second time.
+The actor with first name 'CUBA' should appear in the second SELECT but not the first.*/
