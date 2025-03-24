8a3fc29aa1da8410b6cea4ff6b78ace50e7fb29a
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2025-03-24 10:05:25 -0700

    Update Create Movie table.sql

--- Labs/Create Movie table.sql
+++ Labs/Create Movie table.sql
@@ -5,3 +5,10 @@ Title - variable-length string with up to 50 characters
 Rating - fixed-length string with 4 characters
 ReleaseDate - date
 Budget - decimal value representing a cost of up to 999,999 dollars, with 2 digits for cents*/
+
+create table Movie(
+ID smallint unsigned,
+Title varchar(50),
+Rating char(4),
+ReleaseDate date,
+Budget decimal(8,2));
